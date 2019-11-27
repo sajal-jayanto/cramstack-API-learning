ead92b1809d841201b068a115c0b7829745e98a9
jayanto <sajaljayanto007@gmail.com>
2019-11-28 00:07:21 +0600

    bug fix.

--- src/main/java/com/cramstack/cramstackAPIlearning/Config/SecurityConfig.java
+++ src/main/java/com/cramstack/cramstackAPIlearning/Config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/login").permitAll()
+        http.csrf().disable().authorizeRequests().antMatchers("/login" , "/api/user/create").permitAll()
                 .anyRequest().authenticated().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
